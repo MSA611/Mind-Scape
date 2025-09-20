64ed826c9e57fca57246c568e3c2fd7445fe7401
MSA611 <shoaib9542186823@gmail.com>
2025-09-20 21:31:03 +0530

    changed the url and some scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db.js";
 import router from "./router.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/api/note", router);
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(process.env.port, () => {
