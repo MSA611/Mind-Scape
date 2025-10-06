f038a64cee0ec1b294d5e91c7f53870d7cb241e9
MSA611 <shoaib9542186823@gmail.com>
2025-10-06 20:08:36 +0530

    setted up the authentication system basics

---
+++ backend/src/note.controller.js
@@ -0,0 +1,88 @@
+import Note from "./NoteModel.js";
+
+export const getAllNotes = async (_, res) => {
+  try {
+    const note = await Note.find();
+    res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error In Finding Data" });
+  }
+};
+
+export const CreateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    //checking weather the note is title or content empty
+    if (!title || !content)
+      return res.status(500).json({ message: "Please Fill All The Details" });
+    //checking weather both title and content exists
+
+    const contentExists = await Note.findOne({ content });
+    const titleExists = await Note.findOne({ title });
+
+    if (titleExists && contentExists)
+      return res
+        .status(500)
+        .json({ message: "Both The Title And Content Exists" });
+
+    //check weather the title exists
+    if (titleExists)
+      return res.status(500).json({ message: "This Title Already Exits" });
+
+    //check weather the content exists or not
+    if (contentExists)
+      return res.status(500).json({ message: "This content Already Exists" });
+
+    //saving the note
+    const note = new Note({
+      title: title,
+      content: content,
+    });
+
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error While Create Notes" });
+  }
+};
+
+export const DelNote = async (req, res) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+    res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error While Deleting" });
+  }
+};
+
+export const UpdateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content)
+      return res.status(500).json({ message: "Please Fill All the Details" });
+
+    const note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true },
+    );
+    res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error While Updating" });
+  }
+};
+
+export const getNote = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    return res.status(200).json(note);
+  } catch (error) {
+    console.error(error);
+    res.status.json(500).json({ message: "Error While finding Data" });
+  }
+};
---
+++ backend/src/user.controller.js
@@ -0,0 +1,85 @@
+import User from "./user.model.js";
+import bcrypt from "bcrypt";
+import generateToken from "./generateToken.js";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password, profilePic } = req.body;
+  try {
+    if (!fullName || !email || !password)
+      return res.status(400).json({ message: "Please Fill All The Details" });
+
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ message: "Password Should Be Minimum 6 Characters" });
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email))
+      return res.status(400).json({ message: "Invalid Email" });
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User Already Exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = User({
+      fullName,
+      email,
+      password: hashedPassword,
+      profilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      generateToken(newUser._id, res);
+      res.status(201).json(newUser);
+    } else {
+      res
+        .status(400)
+        .json({ message: "SomeThing went wrong while creation of user" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed To signup the user" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password)
+      return res.status(400).json({ message: "Please Fill All The Details" });
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email))
+      return res.status(400).json({ message: "Invalid Email" });
+
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ message: "Password Should Be Minimum 6 Characters" });
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ message: "Email Already Exists Try Another One" });
+
+    const comparePass = await bcrypt.compare(password, user.password);
+    if (!comparePass)
+      return res.status(400).json({ message: "Wrong Password" });
+
+    res.status(200).json({ message: "User logged In SuccessFully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed To Login The User" });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie("jwt", "", {
+    maxAge: 0,
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
