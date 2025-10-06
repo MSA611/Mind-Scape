2851f0d71419bca45ddb42ee86bf9f5efdf7befe
MSA611 <shoaib9542186823@gmail.com>
2025-10-06 20:19:30 +0530

    added middleware for checking the authentication for the user

---
+++ backend/src/authenticate.user.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import User from "./user.model.js";
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token)
+      return res
+        .status(403)
+        .json({ message: "Token Does Not Exists For The User" });
+
+    const decode = jwt.verify(token, process.env.jwt_secret);
+    if (!decode)
+      return res.status(403).json({ message: "User Has Token But Not Valid" });
+
+    const user = await User.findOne(decode.email).select("-password");
+
+    res.user = user;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: "Failed To Authenticate User" });
+    next(error);
+  }
+};
+
+export default authenticate;
--- backend/src/user.route.js
+++ backend/src/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { login, logout, signup } from "./user.controller.js";
+import authenticate from "./authenticate.user.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.post("/logout", logout);
+userRouter.get("/check", authenticate);
 
 export default userRouter;
